56bdcce944546cbcea4ae36ab51dca24d7bf0aa5
Brayan-724 <bjqf0724@gmail.com>
2021-12-21 21:44:18 -0700

    |Fix| Build command

--- dist/helpers/response.js
+++ dist/helpers/response.js
@@ -1,12 +1,12 @@
 export default ResDef;
 export function ResDef(res, options) {
-    const done = options.status >= 400;
+    const done = options.status <= 400;
     res
         .status(options.status)
         .send({
         done: done,
         status: options.status,
-        data: done ? options.data || {} : null,
-        error: done ? null : options.error || { name: "Unexpected", message: "IDK" }
+        data: done ? (options.data || {}) : null,
+        error: done ? null : (options.error || { name: "Unexpected", message: "IDK" })
     });
 }
